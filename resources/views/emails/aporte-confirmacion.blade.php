f13d35002927ebb2d6d68fc6788e29a9487bf55c
ferares <fermin@ares.uy>
2024-05-14 10:03:13 -0300

    Enviar correo de confirmación al usuario al recibir un aporte

---
+++ resources/views/emails/aporte-confirmacion.blade.php
@@ -0,0 +1,69 @@
+@component('mail::message')
+
+# Nuevo aporte recibido
+
+Gracias por colaborar con Arbolado Urbano.
+
+Tu aporte fue recibido con éxito y será analizado para su integración a nuestro sistema.
+
+Recibirás otro correo una vez realizado el análisis.
+
+Estos son los datos que recibimos:
+
+**Nombre**: {{ $data['name'] }}
+
+**Correo**: <a href="mailto:{{ $data['email'] }}">{{ $data['email'] }}</a>
+
+@if (isset($data['website']) && $data['website'] !== "")
+  **Sitio web**: {{ $data['website'] }}
+@endif
+
+**Datos**:
+
+<ul>
+  <li>
+    <b>Ubicación:</b> <a href="https://maps.google.com/?q={{$data['coordinates']}}">{{$data['coordinates']}}</a>
+  </li>
+  @if (isset($data['height']) && $data['height'] !== "")
+    <li>
+      <b>Altura:</b> {{$data['height']}}m
+    </li>
+  @endif
+  @if (isset($data['diameterTrunk']) && $data['diameterTrunk'] !== "")
+    <li>
+      <b>Diámetro tronco:</b> {{$data['diameterTrunk']}}m
+    </li>
+  @endif
+  @if (isset($data['diameterCanopy']) && $data['diameterCanopy'] !== "")
+    <li>
+      <b>Diámetro copa:</b> {{$data['diameterCanopy']}}m
+    </li>
+  @endif
+  @if (isset($data['inclination']) && $data['inclination'] !== "")
+    <li>
+      <b>Inclinación:</b> {{$data['inclination']}}º
+    </li>
+  @endif
+  @if (isset($data['health']) && $data['health'] !== "")
+    <li>
+      <b>Estado fitosanitario:</b> {{$data['health']}}
+    </li>
+  @endif
+  @if (isset($data['development']) && $data['development'] !== "")
+    <li>
+      <b>Etapa de desarrollo:</b> {{$data['development']}}
+    </li>
+  @endif
+  @if (isset($data['species']) && $data['species'] !== "")
+    <li>
+      <b>Especie:</b> {{$data['species']}}
+    </li>
+  @endif
+  @if (isset($data['speciesId']) && $data['speciesId'] !== "")
+    <li>
+      <b>Especie ID:</b> {{$data['speciesId']}}
+    </li>
+  @endif
+</ul>
+
+@endcomponent
