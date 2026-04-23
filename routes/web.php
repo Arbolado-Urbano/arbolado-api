5cab4a6b25f7a3f984f00afdc7928dd9f1d4f4de
Ferares <fermin@ares.uy>
2026-04-23 10:46:37 -0300

    Create aportes & fuentes controllers

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AportesController;
 use App\Http\Controllers\ArbolesController;
 use App\Http\Controllers\EspeciesController;
+use App\Http\Controllers\FuentesController;
 use App\Http\Controllers\IdentifyController;
 
 Route::get('/', function () {
     return "Arbolado Urbano API V1.0.0";
 });
 
-Route::get('/fuentes/{slug}', [ArbolesController::class, 'getSource']);
+Route::get('/fuentes/{slug}', [FuentesController::class, 'getTrees']);
 Route::get('/especies', [EspeciesController::class, 'list']);
 Route::get('/arboles', [ArbolesController::class, 'list']);
 Route::get('/arboles/{id}', [ArbolesController::class, 'get']);
-Route::post('/arboles', [ArbolesController::class, 'add']);
+Route::post('/arboles', [AportesController::class, 'add']);
 Route::post('/identificar', [IdentifyController::class, 'post']);
