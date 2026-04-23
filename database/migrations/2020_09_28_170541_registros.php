9ca243a1ce7e847a1f642b98881bf4e77e79cd8f
Ferares <fermin@ares.uy>
2026-04-23 12:18:55 -0300

    Update migrations to reflect current db state

--- database/migrations/2020_09_28_170541_registros.php
+++ database/migrations/2020_09_28_170541_registros.php
@@ -15,31 +15,22 @@ class Registros extends Migration
     {
       Schema::create('registros', function (Blueprint $table) {
         $table->increments('id');
-        $table->unsignedInteger('arbol_id');
-        $table->float('lat', 12, 10)->nullable();
-        $table->float('lng', 12, 10)->nullable();
-        $table->string('localidad')->default('CABA');
-        $table->string('calle')->nullable();
-        $table->integer('calle_altura')->nullable();
-        $table->string('espacio_verde')->nullable();
         $table->decimal('altura', 6, 2)->nullable();
         $table->decimal('diametro_a_p', 6, 2)->nullable();
         $table->decimal('diametro_copa', 6, 2)->nullable();
         $table->integer('inclinacion')->nullable();
         $table->string('estado_fitosanitario')->nullable();
         $table->string('etapa_desarrollo')->nullable();
-        $table->string('fuente_codigo_original')->nullable();
+        $table->text('notas')->nullable();
         $table->timestamp('fecha_creacion')->useCurrent();
-        $table->string('removido')->nullable();
-        $table->string('streetview')->nullable();
         $table->unsignedInteger('fuente_id');
-        $table->unsignedInteger('especie_id');
+        $table->unsignedInteger('arbol_id');
         $table->timestamps();
-      });
-
-      Schema::table('registros', function ($table) {
+        });
+        
+        Schema::table('registros', function ($table) {
+        $table->foreign('arbol_id')->references('id')->on('arboles')->onDelete('cascade');
         $table->foreign('fuente_id')->references('id')->on('fuentes')->onDelete('cascade');
-        $table->foreign('especie_id')->references('id')->on('especies')->onDelete('cascade');
       });
     }
 
