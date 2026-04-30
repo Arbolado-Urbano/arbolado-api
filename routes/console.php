5f3b434368277b12d53c9db4d063942cecae73dc
Ferares <fermin@ares.uy>
2026-04-30 15:38:24 -0300

    Cron job for sending daily updates to the admin

--- routes/console.php
+++ routes/console.php
@@ -11,14 +11,12 @@ use App\Mail\Pendientes as PendientesCorreo;
 Schedule::call(function () {
     $aportesCount = count(Aporte::select(['id'])->where('cargado', 0)->get());
     $especiesCount = count(Especie::select(['id'])->whereNull('familia_id')->get());
-    \Log::debug($aportesCount);
-    \Log::debug($especiesCount);
     if ($aportesCount > 0 || $especiesCount > 0) {
         $email = new PendientesCorreo($especiesCount, $aportesCount);
         $email->subject('Revisiones pendientes | Arbolado Urbano');
         try {
             Mail::to(config('mail.admin_email'))->send($email);
-            } catch (\Throwable $th) {
+        } catch (\Throwable $th) {
             \Log::debug($th);
         }
     }
