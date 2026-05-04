b9ce3b75e61ee433a948202df27ce53fa10d9575
Ferares <fermin@ares.uy>
2026-05-04 10:09:47 -0300

    Pendientes email send ids

--- resources/views/emails/pendientes.blade.php
+++ resources/views/emails/pendientes.blade.php
@@ -2,12 +2,40 @@
 
 # Actividades pendientes de revisión
 
-@if ($especiesCount > 0)
-  **Especies**: {{ $especiesCount }}
+@if (count($especies) > 0)
+  <h2>
+    Especies
+  </h2>
+  <div style="margin-left:16px">
+    <b>IDs:</b>
+    <ul>
+      @foreach ($especies as $especie)
+      <li>
+        {{ $especie->id }}
+      </li>
+      @endforeach
+    </ul>
+    <b>Total:</b> {{ count($especies) }}
+  </div>
+  <hr style="margin:16px 0" />
 @endif
 
-@if ($aportesCount > 0)
-  **Aportes**: {{ $aportesCount }}
+@if (count($aportes) > 0)
+  <h2>
+    Aportes
+  </h2>
+  <div style="margin-left:16px">
+    <b>IDs:</b>
+    <ul>
+      @foreach ($aportes as $aporte)
+        <li>
+        {{ $aporte->id }}
+        </li>
+      @endforeach
+    </ul>
+    <b>Total:</b> {{ count($aportes) }}
+  </div>
+  <hr style="margin:16px 0" />
 @endif
 
 @endcomponent
--- routes/console.php
+++ routes/console.php
@@ -9,10 +9,10 @@ use App\Mail\Pendientes as PendientesCorreo;
 
 // Informar al administrador de aportes y especies pendientes de revisión y aprobación
 Schedule::call(function () {
-    $aportesCount = count(Aporte::select(['id'])->where('cargado', 0)->get());
-    $especiesCount = count(Especie::select(['id'])->whereNull('familia_id')->get());
-    if ($aportesCount > 0 || $especiesCount > 0) {
-        $email = new PendientesCorreo($especiesCount, $aportesCount);
+    $aportes = Aporte::select(['id'])->where('cargado', 0)->get();
+    $especies = Especie::select(['id'])->whereNull('familia_id')->get();
+    if (count($aportes) > 0 || count($especies) > 0) {
+        $email = new PendientesCorreo($especies, $aportes);
         $email->subject('Revisiones pendientes | Arbolado Urbano');
         try {
             Mail::to(config('mail.admin_email'))->send($email);
