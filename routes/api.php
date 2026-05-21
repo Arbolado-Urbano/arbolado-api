0c51c4d7868b13027a2f1be14826185b2e745848
Ferares <fermin@ares.uy>
2026-05-21 11:43:44 -0300

    Validate user code endpoint

--- routes/api.php
+++ routes/api.php
@@ -7,15 +7,17 @@ use App\Http\Controllers\ArbolesController;
 use App\Http\Controllers\EspeciesController;
 use App\Http\Controllers\FuentesController;
 use App\Http\Controllers\IdentifyController;
+use App\Http\Controllers\UsuariosController;
 
 Route::get('/', function () {
-    return "Arbolado Urbano API V1.0.0";
+    return "Arbolado Urbano API V1.5.0";
 });
 
 Route::get('/fuentes/{slug}', [FuentesController::class, 'getTrees']);
 Route::get('/especies', [EspeciesController::class, 'list']);
 Route::get('/arboles', [ArbolesController::class, 'list']);
 Route::get('/arboles/{id}', [ArbolesController::class, 'get']);
+Route::post('/usuarios/{code}', [UsuariosController::class, 'codeExists']);
 Route::post('/arboles', [ArbolesController::class, 'add']);
 Route::post('/aportes', [AportesController::class, 'add']);
 Route::post('/identificar', [IdentifyController::class, 'post']);
